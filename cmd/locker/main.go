8af55950c68a4e015702bc7d024777d913237bb8
Dan Lavine <lavineds@gmail.com>
2023-09-28 13:49:45 -0700

    actually run the locker server

--- cmd/locker/main.go
+++ cmd/locker/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/DanLavine/goasync"
 	"github.com/DanLavine/willow/internal/config"
 	"github.com/DanLavine/willow/internal/logger"
+	"github.com/DanLavine/willow/internal/server"
+	"github.com/DanLavine/willow/internal/server/versions/v1server"
 )
 
 func main() {
@@ -16,6 +22,20 @@ func main() {
 
 	logger := logger.NewZapLogger(cfg)
 	defer logger.Sync()
+
+	// setup async handlers
+	//// using strict config ensures that if any process fails, the server will ty and shutdown gracefully
+	taskManager := goasync.NewTaskManager(goasync.StrictConfig())
+
+	// v1 api handlers
+	//// http2 server to handle all client requests
+	taskManager.AddTask("tcp_server", server.NewLockerTCP(logger, cfg, v1server.NewLockHandler(logger)))
+
+	// start all processes
+	shutdown, _ := signal.NotifyContext(context.Background(), syscall.SIGINT)
+	if errs := taskManager.Run(shutdown); errs != nil {
+		log.Fatal("Failed runnng locker cleanly", errs)
+	}
 
 	logger.Info("Successfully shutdown")
 }
